edb0c20f13795d178fab60ce925df1c26c5e3b36
lee mih duwk <sympathic.elf181203@gmail.com>
2024-03-16 21:19:09 +0700

    upload
    assssssss

---
+++ N_Puzzle_Game/Start_Window.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N_Puzzle_Game
+{
+    public partial class Start_Window : Form
+    {
+        public static Start_Window __main__;
+        bool Eight = false, Fifteen = false;
+        public Start_Window()
+        {
+            InitializeComponent();
+            __main__ = this;
+        }
+
+        private void Start_Window_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbx_size.SelectedItem.ToString())
+            {
+                case "3 * 3":
+                    comboBox2.Items.Clear();
+                    List<string> lst = new List<string>() { "BFS", "A*", "Bidirectional Search", "Greedy Best First Search" };
+                    foreach (string s in lst) comboBox2.Items.Add(s);
+                    Eight = true; Fifteen = false;
+                    break;
+                case "4 * 4":
+                    comboBox2.Items.Clear();
+                    List<string> _lst = new List<string>() { "A*", "Greedy Best First Search", "Bidirectional Search" };
+                    foreach (string s in _lst) comboBox2.Items.Add(s);
+                    Eight = false; Fifteen = true;
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Eight_Puzzle _8 = new Eight_Puzzle();
+            Fifteen_Puzzle _15 = new Fifteen_Puzzle();
+            if (Eight)
+            {
+                switch (comboBox2.SelectedItem.ToString())
+                {
+                    case "A*":
+                        _8.b_a_star = true;
+                        break;
+                    case "BFS":
+                        _8.b_bfs = true;
+                        break;
+                    case "Greedy Best First Search":
+                        _8.b_gbfs = true;
+                        break;
+                    case "Bidirectional Search":
+                        _8.b_bi = true;
+                        break;
+                }
+                _8.Show();
+                __main__.Hide();
+            }
+            else if (Fifteen)
+            {
+                switch (comboBox2.SelectedItem.ToString())
+                {
+                    case "A*":
+                        _15.b_a_star = true;
+                        break;
+                    case "Greedy Best First Search":
+                        _15.b_gbfs = true;
+                        break;
+                    case "Bidirectional Search":
+                        _15.b_bi = true;
+                        break;
+                }
+                _15.Show();
+                __main__.Hide();
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# tomtep
- Tom ist ich und du bist Tep, wir sind auf der gleichen Ebene
+# Artificial Intelligence 8/15 Puzzle Game
+
+## Algorithms :
+       1. Breadth first search.
+       2. A*.
+       3. Greedy best first search.
+       4. Bidirectional search (meet in middle).
+## Data structures :
+       1. Trie.
+       2. Priority Queue & Queue.
+       3. Stack.
+       4. Set.
+       5. Dictionary.
+---
+### See Video on Youtube [here](https://www.youtube.com/watch?v=1DyJ6NtVp3k&t=1s)
+---
+## License
+copyright © 2017, code released under the [MIT License](LICENSE)
